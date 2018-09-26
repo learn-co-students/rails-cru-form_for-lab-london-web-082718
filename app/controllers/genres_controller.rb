a0603c312df27945d264b4d7feaddbdfe131cf59
github username <github email address>
2018-09-26 16:06:24 +0100

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,33 @@
+class GenresController < ApplicationController
+  before_action :set_genres, only: [:show, :edit, :update]
+
+
+  def show
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+
+  def update
+    @genre.update(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+
+  def set_genres
+    @genre = Genre.find(params[:id])
+  end
+
+  def genre_params(*args)
+    params.require(:genre).permit(*args)
+  end
+
+end
